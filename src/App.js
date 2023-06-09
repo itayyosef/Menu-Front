b2b0144221b51643f089ae50b2af6270cc97f1c2
Itay <itayking12345@gmail.com>
2023-06-09 19:31:17 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
             setCategories(response.data)
         })
     },[])
+    
   return (
     <div>
     <Routes>
--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -48,61 +48,60 @@ function Menu({categories}) {
                 </AppBar>
             </Box>
             <Box sx={{ display: 'flex' }}>
-            <Drawer 
-                variant="permanent" 
-                anchor="left" 
-                sx={{
-                    width: drawerWidth,
-                    flexShrink: 0,
-                    '& .MuiDrawer-paper': { 
-                        width: drawerWidth, 
-                        boxSizing: 'border-box',
-                    },
-                }}
-            > 
-            <Toolbar>All Categories</Toolbar>
-            <Divider />
-            <List sx={{display: 'flex',flexDirection: 'column',alignItems: 'center',height: '100%',justifyContent: 'center'}}>
-                <div style={{fontWeight:"bold",fontSize:"17px"}}>
-                    {'Jump Between Categories'.toUpperCase()}
-                </div>
-                { categories.map((category) => (
-                    <ListItem key={category.id} disablePadding>
-                        <ListItemButton 
-                            onClick={()=>navigate(`/category/${category.id}`)} 
-                            sx={{
-                                display: 'flex',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                color: params.id == category.id ? 'red' : 'inherit',
-                            }}
-                        >
-                            <Box>
-                                <img
-                                    src={category.image_url} 
-                                    alt={category.category_name} 
-                                    style={{width:125,height:47,borderRadius:'20%'}}
-                                />
-                                <ListItemText 
-                                    primary={
-                                        <Typography 
-                                            variant="body1" 
-                                            component="div"
-                                            sx={{
-                                                textAlign: 'center',
-                                                marginTop: '4px',
-                                                mb :"3px",
-                                                fontWeight: params.id == category.id ? 'bold' : 'normal'
-                                            }}>
-                                            {category.category_name}
-                                        </Typography>
-                                    }
-                                />
-                            </Box>
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
+                <Drawer 
+                    variant="permanent" 
+                    anchor="left" 
+                    sx={{
+                        width: drawerWidth,
+                        flexShrink: 0,
+                        '& .MuiDrawer-paper': { 
+                            width: drawerWidth, 
+                            boxSizing: 'border-box',
+                        },
+                    }}
+                > 
+                <Toolbar>All Categories</Toolbar>
+                <List sx={{display: 'flex',flexDirection: 'column',alignItems: 'center',height: '100%',justifyContent: 'center'}}>
+                    <div style={{fontWeight:"bold",fontSize:"17px"}}>
+                        {'Jump Between Categories'.toUpperCase()}
+                    </div>
+                    { categories.map((category) => (
+                        <ListItem key={category.id} disablePadding>
+                            <ListItemButton 
+                                onClick={()=>navigate(`/category/${category.id}`)} 
+                                sx={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    color: params.id == category.id ? 'red' : 'inherit',
+                                }}
+                            >
+                                <Box>
+                                    <img
+                                        src={category.image_url} 
+                                        alt={category.category_name} 
+                                        style={{width:125,height:47,borderRadius:'20%'}}
+                                    />
+                                    <ListItemText 
+                                        primary={
+                                            <Typography 
+                                                variant="body1" 
+                                                component="div"
+                                                sx={{
+                                                    textAlign: 'center',
+                                                    marginTop: '4px',
+                                                    mb :"3px",
+                                                    fontWeight: params.id == category.id ? 'bold' : 'normal'
+                                                }}>
+                                                {category.category_name.toLocaleUpperCase(undefined,{usage:'title'})}
+                                            </Typography>
+                                        }
+                                    />
+                                </Box>
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
                 </Drawer>
             <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}>
                 <Toolbar />
@@ -118,39 +117,39 @@ function Menu({categories}) {
                 <Grid container spacing={1}>
                     {dishes.map((dish,index) => (
                         <>
-                        <Grid item key={dish.id} xs={12} sm={5.9}>
-                            <Card style={{ border: "none", boxShadow: "none" ,height:440}}>
-                                <CardMedia
-                                sx={{height:240}}
-                                image={dish.imageURL}
-                                title={dish.dish_name}
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div" sx={{textAlign:"center"}}>
-                                        <div style={{fontWeight:"bold"}}>
-                                            {dish.dish_name}
-                                        </div>
+                            <Grid item key={dish.id} xs={12} sm={5.9}>
+                                <Card style={{ border: "none", boxShadow: "none" ,height:440}}>
+                                    <CardMedia
+                                        sx= {{height:240}}
+                                        image={dish.imageURL}
+                                        title={dish.dish_name}
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div" sx={{textAlign:"center"}}>
+                                            <div style={{fontWeight:"bold"}}>
+                                                {dish.dish_name}
+                                            </div>
+                                        </Typography>
+                                        <Typography cariant="body2" color="text.secondary">
+                                            {dish.description}
+                                        </Typography>
+                                    </CardContent>
+                                    <Typography sx={{textAlign:"center",fontWeight:'bold'}}>
+                                        {dish.price} ₪
                                     </Typography>
-                                    <Typography cariant="body2" color="text.secondary">
-                                        {dish.description}
-                                    </Typography>
-                                </CardContent>
-                                <Typography sx={{textAlign:"center",fontWeight:'bold'}}>
-                                    {dish.price} ₪
-                                </Typography>
-                                <Divider sx={{margin:'16px 0'}}/>
-                            </Card>
-                        </Grid>
-                        {index !== dishes.length - 1 && (
-                            <Grid item>
-                                <Divider variant="middle" orientation='vertical'/>
+                                    <Divider sx={{margin:'16px 0'}}/>
+                                </Card>
                             </Grid>
+                            {index !== dishes.length - 1 && (
+                                <Grid item>
+                                    <Divider variant="middle" orientation='vertical'/>
+                                </Grid>
                             )}
                         </>
                     ))}
                 </Grid>
             </Box>
-        </Box>
+            </Box>
         </div>
     )
 }
